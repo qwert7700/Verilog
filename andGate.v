bf50bdaa91b01315152a1ef7e0cdcc8701801610
qwert7700 <smhquadri99@gmail.com>
2024-04-11 14:22:58 -0700

    adding another comment

---
+++ andGate.v
@@ -0,0 +1,18 @@
+
+
+module andGate (
+    input wire a,
+    input wire b,
+
+    output wire c
+);
+
+initial begin 
+    a = 0 ;
+    b = 0 ;
+    c = 0 ;
+
+end 
+//Adding changes to a branch
+
+//adding changes to a branch again
